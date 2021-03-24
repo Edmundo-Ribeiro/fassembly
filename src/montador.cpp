25d9b0ca275f5ba7cab2a3d23459bfbbd6da9db1
Edmundo-Ribeiro <jtvedy@gmail.com>
2021-03-24 00:06:42 -0300

    funcao classificadora, primeira passagem completa e identificacao de erros

--- src/montador.cpp
+++ src/montador.cpp
@@ -1,49 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <regex>
-
+#include "../include/errors.h"
 using namespace std;
 
 /**
-Algoritmo da primeira passagem:
-contador_posição = 0
-contador_linha = 1
-Enquanto arquivo fonte não chegou ao fim, faça:
-  Obtém uma linha do fonte
-  Separa os elementos da linha:
-  rótulo, operação, operandos, comentários
-  
-  Ignora os comentários
-  
-  Se existe rótulo:
-    Procura rótulo na TS (Tabela de Símbolos)
-  
-  Se achou: Erro à símbolo redefinido
-  
-  Senão: Insere rótulo e contador_posição na TS
-    Procura operação na tabela de instruções
-  Se achou:
-    contador_posição = contador_posição + tamanho da instrução
-  Senão:
-    Procura operação na tabela de diretivas
-  Se achou:
-    chama subrotina que executa a diretiva
-    contador_posição = valor retornado pela subrotina
-  Senão: Erro, operação não identificada
-  contador_linha = contador_linha + 1
+ * Erro: numero de operandos na operação ok
+ * Erro: não inteiro em const ok
+ * Erro: regras da label ok
+ * Erro: redefinição de rotulo ok
+ * Erro: Sem vigurla em COPY ok;
+ * Erro: operação não existe 
+ * Erro: declarações e rótulos ausentes;
 */
+
+
+
+
 
  //Tabela de operações
   // string --> Opcode, numero_de_operandos
-
-
 typedef map<string,array<int,2>> operation_table;
+
 typedef map<string,int> symbols_table;
 
-  constexpr bool OPCODE = 0;
+typedef vector<string>::iterator vsit;
+
+constexpr bool OPCODE = 0;
  
- 
- operation_table OT { {"ADD", {1, 1}},
+  
+symbols_table ST;
+
+operation_table OT { 
+    {"ADD", {1, 1}},
     {"SUB", {2, 2}},
     {"MUL", {3, 2}},
     {"DIV", {4, 2}},
@@ -61,76 +50,126 @@ typedef map<string,int> symbols_table;
     {"CONST", {-1, 1}}
   };
 
-string errors;
+//guardar erros
+ERRORS e; 
 
-bool is_label(vector<string>::const_iterator it, vector<string> &v){
-  return it != v.end() - 1 && *(it+1) == ":";
-}
+//Nome já diz, contador de linha e contador de posição
+int line_counter = 1, position_counter = 0;
 
-bool is_operation(string token, operation_table &ot){
-   return ot.find(token) != ot.end();
-}
-
-bool is_label_defined(string token, symbols_table &st){
-   return st.find(token) != st.end();
-}
-
-typedef vector<string>::iterator vsit;
-
-bool is_label_definition(vsit it, vsit itend){
-  return it != itend - 1 && *(it+1) == ":";
-}
-
-string classifier(vector<string> tokens);
-
-string check_for_next_n_operators(int n, vsit it, vsit itend){
-  vector<string> aux(it+1,itend);
-  string operands = classifier(aux);
-  if(n == 2 && (operands == "P,P" || operands == "P,PC") ){
-    return operands;
-  }
-  if(n == 1 && (operands == "P" || operands == "PC")) {
-    return operands;
-  }
-  return ""; //jogar erros de numero de operandos
-}
-
+//Verificar se string é um numero inteiro
 bool is_int(string s){
   auto start = (s[0] == '-' ? s.begin() + 1 : s.begin());
 
   for(auto c = start; c != s.end(); ++c){
-    if(*c > '9' || *c < '0') return false;//lançar erro
-
+    if(*c > '9' || *c < '0') return false; 
   }
   return true;
 }
 
+//verificar as regras de construção de uma label
+bool is_label_ok(string label){
+  string validChar("_$");
+  bool flag = true;
+  if(label[0] >= '0' && label[0]  <= '9') {
+    flag = false;
+    e.add(e.LEXICO, line_counter,"{" + label +"} Label não pode iniciar com números");   
+  }
+  if(label.size() > 50){
+    flag = false;
+    e.add(e.LEXICO, line_counter,"{" + label +"} Label não pode ser maior que 50 caracteres");   
+  }
+
+  for (auto it = label.begin(); it != label.end(); ++it ){
+    if( (*it < 'A' || *it > 'Z') && (*it < '0' || *it > '9') && *it != '_'){
+      flag = false;
+      e.add(e.LEXICO, line_counter,"{" + label +"} Label não pode conter caracteres não alfanúmericos exeto \"_\" ");
+      break;
+    } 
+  }
+
+  return flag;
+}
+
+//Verificar se token é uma operação
+bool is_operation(string token, operation_table &ot){
+   return ot.find(token) != ot.end();
+}
+//Verificar se label já está definida
+bool is_label_defined(string token, symbols_table &st){
+   return st.find(token) != st.end();
+}
+
+//Vefificar se está ocorrendo uma definicação de label 
+bool is_label_definition(vsit it, vsit itend){
+  return it != itend-1 && *(it+1) == ":";
+}
+
+
+
+string classifier(vector<string> tokens);
+string check_for_next_n_operators(int n, vsit it, vsit itend);
+
+//verificar se apos operação o numero certo de operandos está presente
+string check_for_next_n_operators(int n, vsit it, vsit itend){
+  vector<string> aux(it+1,itend);
+  string operands = classifier(aux);
+  if(n == 2 ){
+    if(operands == "P,P" || operands == "P,PC")
+      return operands;
+    else if (operands == "PP")
+        e.add(e.LEXICO, line_counter,"Está faltando \",\" entre os operandos da operação {" + *it +"}");
+
+  }
+  if(n == 1 && (operands == "P" || operands == "PC")) {
+    return operands;
+  }
+  
+  e.add(e.LEXICO, line_counter,"Número indevidos de operandos. Operação {" + *it +"} requer " +to_string(n) + " operando" + (n > 1? "s" : ""));
+
+  return ""; //jogar erros de numero de operandos
+}
+
+
+//cria string classificando os tokens (tbm verifica erros e cria tabela de simbolos)
 string classifier(vector<string> tokens){
   string anottend;
   int n_ops = 0;
 
-  cout << "Token: " ;
   for(auto it = tokens.begin(); it != tokens.end(); ++it){
-    cout << *it << " ";
     if(is_label_definition(it, tokens.end())){
-      //checkRules(label,*it)
+      if(is_label_ok(*it)){
+        if(!is_label_defined(*it,ST)) 
+          ST[*it] = position_counter;
+        else
+          e.add(e.SEMANTICO,line_counter,"Redefinição de simbolo { " + *it + " }.");
+      }
       anottend += "L";
       ++it;
     }
     else if(is_operation(*it,OT)){
-      if(*it == "CONST"){
-        is_int(*(it+1));
-        n_ops = 1;
-      }
-      n_ops += OT[*it][!OPCODE] - 1;
-      anottend+= ("O" + to_string(n_ops));
-      if(n_ops != 0){
-          string res = check_for_next_n_operators(n_ops,it,tokens.end());
-          anottend += res;
-          it=tokens.end()-1;
+        if(*it == "CONST"){
+          if(is_int(*(it+1))){
+            n_ops = 1;
+          }
+          else{
+            e.add(e.SINTATICO,line_counter,"Diretiva CONST requer número inteiro como parametro, não {" + *(it+1) + "}");
+          }
+        }
+        
+        position_counter += OT[*it][!OPCODE];
+        n_ops += OT[*it][!OPCODE] - 1;
 
-      }
+        anottend += ("O" + to_string(n_ops));
+        
+        if(n_ops != 0){
+            string res = check_for_next_n_operators(n_ops,it,tokens.end());
+            anottend += res;
+            it=tokens.end()-1;
+        }
     }
+      // else{
+      //   e.add(e.SINTATICO,line_counter,"Operação ( " + *it + ") não existe.");
+      // }
     else if(*it == ";"){
       anottend+= "C";
       break;
@@ -142,19 +181,11 @@ string classifier(vector<string> tokens){
       anottend += "P";
     }
   }
-  if(anottend.size()){
-
-    cout << "Anotted: ";
-    for(auto i = anottend.begin(); i != anottend.end();++i){
-      cout << *i;
-    }
-    cout << endl;
-  }
   return anottend;
 
 }
 
-
+//De uma linha do arquivo fonte, separa todos os tokens
 vector<string>get_tokens_from_line(string line){
 
     vector<string> tokens; //vetor para retorno
@@ -176,7 +207,7 @@ vector<string>get_tokens_from_line(string line){
           } 
           else{
             tokens.push_back(aux); //adicionar aux ao vetor
-            if (!(*it == ' ' || it+1 == line.end())) tokens.push_back(string(1,*it)); // se o separador não for espaço e nem o ultimo caracter, adicinar o separador no vetor
+            if ((!(*it == ' '  || (it+1) == line.end()) )|| *it ==':') tokens.push_back(string(1,*it)); // se o separador não for espaço e nem o ultimo caracter, adicinar o separador no vetor
 
           }
           aux.clear(); // resetar aux
@@ -188,79 +219,68 @@ vector<string>get_tokens_from_line(string line){
   }
 
 
+void first_pass(string filename){
+  string line;
+  string abbreviation;
+  vsit it;
+  ifstream source(filename);
+  ofstream temp("temp" + filename);
+
+  if(!source.is_open()){
+    cout << "error trying to open " << filename << endl;
+  }
+
+  while (!source.eof()){
+    getline(source, line);
+    
+    auto tokens = get_tokens_from_line(line);
+
+    if(tokens.empty()){
+      ++line_counter;
+      temp << endl;
+      continue;
+    } 
+
+    abbreviation = classifier(tokens);
+    ++line_counter;
+
+    it = tokens.begin();
+    for(auto c = abbreviation.begin(); c != abbreviation.end(); ++c){
+
+        switch (*c){
+          case 'L': it+=2; break;
+          
+          case 'O': ++c; temp << *it << "\t"; ++it;break;
+          
+          case 'C': break;
+          
+          default:
+            temp << *it << "\t";
+            it += it+1 == tokens.end() ? 0 : 1;
+            break;
+        }
+    }
+    temp << endl;
+  }
+    source.close();
+    temp.close();
+}
+
 int main(int argc,char **argv){
   string fname = string(argv[1]!= NULL ? argv[1] : "");
   string line;
 
   
-  symbols_table ST;
-
   
   if(fname!=""){
 
-    ifstream source(fname);
+    
+    first_pass(fname);
     ofstream destiny(fname.replace(fname.end()-3 ,fname.end(), "obj"));
-    if(!source.is_open()){
-      cout << "error trying to open " << fname << endl;
-    }
-
-    int line_counter = 1, position_counter = 0;
-    while (!source.eof()){
-      getline(source, line);
-      
-      // destiny << line << endl;
-      
-      auto tks = get_tokens_from_line(line);
-
-
-
-
-      if(!tks.empty()){
-        classifier(tks);
-
-      }
-      for(auto it = tks.begin(); it != tks.end(); ++it){
-
-        if(is_label(it,tks)){
-          cout << "LABEL(" << *it << ")" << endl;
-          if(!is_label_defined(*it,ST)){
-            ST[*it] = position_counter;
-          }
-          else{
-            stringstream message;
-            message << "\nErro <Tipo>: Definição repetida de simbolo " << position_counter << "\t" << line_counter;
-            errors.append(message.str());
-          }
-
-        }
-        else if(is_operation(*it,OT)){
-          position_counter += OT[*it][!OPCODE];
-        }
-        else{
-          stringstream message;
-          message << "\nErro <Tipo>: Operação ( " << *it << ") não existe. "<<position_counter << " " << line_counter;
-          errors.append(message.str());
-        }    
-
-        ++line_counter;     
-      }
-
-      // cout << line << " ->> ";
-			// it = 	sregex_token_iterator(line.begin(), line.end(), re);
-			// for (; it != reg_end ; ++it) {
-			// 	token = it->str();
-
-      //   if(is_label(token)) cout << "LABEL("<<token<<")  " ;
-      //   else if(is_operation(token,OT)) cout << "OPERATION("<<token<<")  " ;
-      //   else cout << it->str() << "\t";
-			// }
-			// cout << endl;
-    }
-    source.close();
     destiny.close();
-
-    // for (const auto& m : ST)
-    //   cout << "m[" << m.first << "] = (" << m.second << ") " << endl;
+    if(e.any) e.show();
+    for (const auto& m : ST)
+      cout << "m[" << m.first << "] = (" << m.second << ") " << endl;
 
   }
   return 0;
